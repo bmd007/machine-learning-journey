3ec157524213ddefc4f74d8789f2b043d31bd0e0
Mahdi <bmd579@gmail.com>
2024-06-20 23:47:28 +0200

    initial

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
 print("hi, there!")
+np.zeros(5)
+
+plt.plot([1, 2, 4, 9, 5, 3])
+plt.show()
+
+s = pd.Series([2, -1, 3, 5])
+np.exp(s)
